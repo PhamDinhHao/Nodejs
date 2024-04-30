e156a6583dcedd54700fdab681f08c37487294a3
Your Name <you@example.com>
2024-04-30 10:11:01 -0700

    30/4

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -56,10 +56,27 @@ let getDetailDoctorById = async (req, res) => {
         })
     }
 }
+let bulkCreateSchedule = async (req, res) => {
+    try {
+
+        let infor = await doctorService.bulkCreateSchedule(req.body)
+        return res.status(200).json(
+            infor
+        )
+
+    } catch (error) {
+        console.log(error)
+        return res.status(200).json({
+            errCode: -1,
+            message: "Error from server..."
+        })
+    }
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDocters: getAllDocters,
     postInforDoctor: postInforDoctor,
-    getDetailDoctorById: getDetailDoctorById
+    getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule,
 
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -2,6 +2,9 @@ import { DATE, Model, where } from "sequelize";
 import db from "../models/index";
 import { raw } from "body-parser";
 import { escape } from "mysql2";
+import _ from 'lodash';
+require('dotenv').config();
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -94,6 +97,7 @@ let saveDetailInforDoctor = (inputData) => {
 let getDetailDoctorById = (inputid) => {
     return new Promise(async (resolve, reject) => {
         try {
+
             if (!inputid) {
                 resolve({
                     errCode: 1,
@@ -136,9 +140,66 @@ let getDetailDoctorById = (inputid) => {
         }
     })
 }
+let bulkCreateSchedule = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required param !'
+                })
+            }
+            else {
+                let schedule = data.arrSchedule;
+                if (schedule && schedule.length > 0) {
+                    schedule = schedule.map(item => {
+                        item.maxNumber = MAX_NUMBER_SCHEDULE; //them thuoc tinh cho item
+                        return item
+                    })
+                }
+
+                //get existing all data
+                let existing = await db.Schedule.findAll({
+                    where: { doctorId: data.doctorId, date: data.formatedDate },
+                    attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
+                    raw: true
+                })
+
+                //convert date
+                if (existing && existing.length > 0) {
+                    existing = existing.map(item => {
+                        item.date = new Date(item.date).getTime();
+                        return item
+                    })
+                }
+
+
+                //compare differrent
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date;
+                });
+
+                //create data
+                if (toCreate && toCreate.length > 0) {
+                    await db.Schedule.bulkCreate(toCreate)
+                }
+
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK'
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
     getDetailDoctorById: getDetailDoctorById,
+    bulkCreateSchedule: bulkCreateSchedule
 }
